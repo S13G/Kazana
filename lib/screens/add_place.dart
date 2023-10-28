8776e31bcde6d81a14627cc1cbebcad2b51ca895
s13g <ayflix0@gmail.com>
2023-10-28 17:01:34 +0100

    implemented location feature

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kazana/providers/user_places.dart';
 import 'package:kazana/widgets/image_input.dart';
+import 'package:kazana/widgets/location_input.dart';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
@@ -64,6 +65,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 _selectedImage = image;
               },
             ),
+            const SizedBox(height: 16),
+            const LocationInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
