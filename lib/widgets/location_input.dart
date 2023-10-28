e91d3284b34ae2c68be6b7d6ad248dd29fa0bb90
s13g <ayflix0@gmail.com>
2023-10-28 18:12:02 +0100

    using geocoding api

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,114 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() {
+    return _LocationInput();
+  }
+}
+
+class _LocationInput extends State<LocationInput> {
+  Location? _pickedLocation;
+
+  var _isGettingLocation = false;
+
+  void _getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    setState(() {
+      _isGettingLocation = true;
+    });
+
+    locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final long = locationData.longitude;
+
+    final url = Uri.parse('https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$long&key=YOUR_API_KEY');
+
+    final response = await http.get(url);
+
+    final responseData = json.decode(response.body);
+
+    final address = responseData['results'][0]['formatted_address'];
+
+    setState(() {
+      _isGettingLocation = false;
+    });
+
+
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget previewContent = Text(
+      'No location chosen',
+      textAlign: TextAlign.center,
+      style: Theme.of(context)
+          .textTheme
+          .bodyLarge!
+          .copyWith(color: Theme.of(context).colorScheme.onBackground),
+    );
+
+    if (_isGettingLocation) {
+      previewContent = const CircularProgressIndicator();
+    }
+
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+            ),
+          ),
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          child: previewContent,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+              onPressed: _getCurrentLocation,
+              icon: const Icon(Icons.location_on),
+              label: const Text('Get current location'),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.map),
+              label: const Text('Select on map'),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
