af5f777aa9d92c2771e8b92616e0532ba2a31c46
s13g <ayflix0@gmail.com>
2023-10-29 12:21:38 +0100

    storing data locally

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -7,6 +7,7 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
   void addPlace(String title, File image, PlaceLocation location) {
+    final
     final newPlace = Place(title: title, image: image, location: location);
     state = [newPlace, ...state]; // new place getting added to the beginning
   }
